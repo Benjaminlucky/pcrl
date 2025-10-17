242c5e3c9ceff8bf5627caa589078b96ddc4a299
@benjamin lucky <bamidelebenjamin5@gmail.com>
2025-10-17 14:41:58 +0100

    MongoDB Setup Implemented

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDb"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello PCRG Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
---
+++ server/note.md
@@ -0,0 +1,3 @@
+ycbAbRwCSFiK4208
+bamideleAdmin
+mongodb+srv://bamideleAdmin:<db_password>@cluster0.l6wrdte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
